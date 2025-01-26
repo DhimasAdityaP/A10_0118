31821e51eb90d6151afee9a70709a4d7ff5e8feb
DhimasAdityaP <dimasadit0105@gmail.com>
2025-01-26 13:13:38 +0700

    add dataclass AktivitasPertanian

--- app/src/main/java/com/example/a10_118/Model/AktivitasPertanian.kt
+++ app/src/main/java/com/example/a10_118/Model/AktivitasPertanian.kt
@@ -1,3 +1,18 @@
 package com.example.a10_118.Model
 
-data class AktivitasPertanian()
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class AktivitasPertanian(
+    @SerialName("id_Aktifitas")
+    val idAktifitas: String,
+    @SerialName("id_tanaman")
+    val idTanaman: String,
+    @SerialName("id_pekerja")
+    val idPekerja: String,
+    @SerialName("tanggal_aktifitas")
+    val tanggalPanen: String,
+    @SerialName("deskripsi_aktifitas")
+    val deskripsiAktifitas: String,
+)

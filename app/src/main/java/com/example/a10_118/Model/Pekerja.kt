9a2ad29f38c1a604ba29c489df4c687e71c2f77c
DhimasAdityaP <dimasadit0105@gmail.com>
2025-01-26 13:11:08 +0700

    add dataclass Pekerja

--- app/src/main/java/com/example/a10_118/Model/Pekerja.kt
+++ app/src/main/java/com/example/a10_118/Model/Pekerja.kt
@@ -1,3 +1,15 @@
 package com.example.a10_118.Model
 
-data class Pekerja()
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class Pekerja(
+    @SerialName("id_pekerja")
+    val idPekerja: String,
+    @SerialName("nama_pekerja")
+    val namaPekerja: String,
+    val jabatan: String,
+    @SerialName("kontak_pekerja")
+    val kontakPekerja: String
+)

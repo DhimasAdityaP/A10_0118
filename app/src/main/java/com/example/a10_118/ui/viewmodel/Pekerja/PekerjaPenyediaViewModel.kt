19be2b6b3d13954bd72266adec13ead04475deba
DhimasAdityaP <dimasadit0105@gmail.com>
2025-01-28 09:28:34 +0700

    add code di PekerjaPenyediaViewModel

--- app/src/main/java/com/example/a10_118/ui/viewmodel/Pekerja/PekerjaPenyediaViewModel.kt
+++ app/src/main/java/com/example/a10_118/ui/viewmodel/Pekerja/PekerjaPenyediaViewModel.kt
@@ -1,2 +1,29 @@
 package com.example.a10_118.ui.viewmodel.Pekerja
 
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewmodel.CreationExtras
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+import com.example.a10_118.PertanianApplication
+
+object PekerjaPenyediaViewModel {
+    val Factory = viewModelFactory {
+        initializer {
+            HomePekerjaViewModel(
+                aplikasiPertanian()
+                    .container.pekerjaRepository
+            )
+        }
+
+        initializer {
+            InsertPekerjaViewModel(
+                aplikasiPertanian()
+                    .container.pekerjaRepository
+            )
+        }
+    }
+
+    fun CreationExtras.aplikasiPertanian(): PertanianApplication =
+        (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as PertanianApplication)
+
+}

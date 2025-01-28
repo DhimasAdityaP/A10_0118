7f56948ca05edafeb38ba3aad91bcdb605ef5178
DhimasAdityaP <dimasadit0105@gmail.com>
2025-01-28 09:36:18 +0700

    add code di CatatanPenyediaViewModel

--- app/src/main/java/com/example/a10_118/ui/viewmodel/Catatan/CatatanPenyediaViewModel.kt
+++ app/src/main/java/com/example/a10_118/ui/viewmodel/Catatan/CatatanPenyediaViewModel.kt
@@ -1,2 +1,29 @@
 package com.example.a10_118.ui.viewmodel.Catatan
 
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewmodel.CreationExtras
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+import com.example.a10_118.PertanianApplication
+
+object CatatanPenyediaViewModel {
+    val Factory = viewModelFactory {
+        initializer {
+            HomeCatatanViewModel(
+                aplikasiPertanian()
+                    .container.catatanRepository
+            )
+        }
+
+        initializer {
+            InsertCatatanViewModel(
+                aplikasiPertanian()
+                    .container.catatanRepository
+            )
+        }
+    }
+
+    fun CreationExtras.aplikasiPertanian(): PertanianApplication =
+        (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as PertanianApplication)
+
+}

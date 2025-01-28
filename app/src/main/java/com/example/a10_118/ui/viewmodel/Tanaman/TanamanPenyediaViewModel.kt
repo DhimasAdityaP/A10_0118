5cd0180f703e6236bc81a62676e1feb9376ddf71
DhimasAdityaP <dimasadit0105@gmail.com>
2025-01-28 08:55:42 +0700

    add code in TanamanPenyediaViewModel

--- app/src/main/java/com/example/a10_118/ui/viewmodel/Tanaman/TanamanPenyediaViewModel.kt
+++ app/src/main/java/com/example/a10_118/ui/viewmodel/Tanaman/TanamanPenyediaViewModel.kt
@@ -1,3 +1,30 @@
 package com.example.a10_118.ui.viewmodel.Tanaman
 
-object TanamanPenyediaViewModel {}
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewmodel.CreationExtras
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+import com.example.a10_118.PertanianApplication
+
+
+object TanamanPenyediaViewModel {
+    val Factory = viewModelFactory {
+        initializer {
+            HomeTanamanViewModel(
+                aplikasiPertanian()
+                    .container.tanamanRepository
+            )
+        }
+
+        initializer {
+            InsertTanamanViewModel(
+                aplikasiPertanian()
+                    .container.tanamanRepository
+            )
+        }
+    }
+
+    fun CreationExtras.aplikasiPertanian(): PertanianApplication =
+        (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as PertanianApplication)
+
+}

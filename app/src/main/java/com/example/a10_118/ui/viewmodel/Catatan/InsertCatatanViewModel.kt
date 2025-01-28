f332cecd71f2f2e263239db3444a6e41fddaea1f
DhimasAdityaP <dimasadit0105@gmail.com>
2025-01-28 09:34:26 +0700

    add code di InsertCatatanViewModel

--- app/src/main/java/com/example/a10_118/ui/viewmodel/Catatan/InsertCatatanViewModel.kt
+++ app/src/main/java/com/example/a10_118/ui/viewmodel/Catatan/InsertCatatanViewModel.kt
@@ -1,2 +1,56 @@
 package com.example.a10_118.ui.viewmodel.Catatan
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.a10_118.Model.CatatanPanen
+import com.example.a10_118.Repository.CatatanRepository
+import kotlinx.coroutines.launch
+
+class InsertCatatanViewModel(private val catatan: CatatanRepository) : ViewModel() {
+    var uiState by mutableStateOf(InsertCatatanUiState())
+        private set
+
+    fun updateInsertCatatanState(insertUiEvent: InsertCatatanUiEvent) {
+        uiState = InsertCatatanUiState(insertCatatanUiEvent = insertUiEvent)
+    }
+
+    fun insertCatatan() {
+        viewModelScope.launch {
+            uiState = uiState.copy(isLoading = true)
+            try {
+                catatan.insertCatatan(uiState.insertCatatanUiEvent.toCatatan())
+                uiState = uiState.copy(isSuccess = true, isLoading = false) // Update state success
+            } catch (e: Exception) {
+                uiState = uiState.copy(isLoading = false) // Handle error
+                e.printStackTrace()
+            }
+        }
+    }
+}
+
+data class InsertCatatanUiState(
+    val insertCatatanUiEvent: InsertCatatanUiEvent = InsertCatatanUiEvent(),
+    val isLoading: Boolean = false,
+    val isSuccess: Boolean = false
+)
+
+data class InsertCatatanUiEvent(
+    val idPanen: String = "",
+    val idTanaman: String = "",
+    val tanggalPanen: String = "",
+    val jumlahPanen: String = "",
+    val keterangan: String = "",
+)
+
+fun InsertCatatanUiEvent.toCatatan(
+
+): CatatanPanen = CatatanPanen(
+    idPanen = idPanen,
+    idTanaman = idTanaman,
+    tanggalPanen = tanggalPanen,
+    jumlahPanen = jumlahPanen,
+    keterangan = keterangan,
+)

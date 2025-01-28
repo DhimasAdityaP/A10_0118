fe679b1231b3cbcc082836a3cbe351eeecdc5fd4
DhimasAdityaP <dimasadit0105@gmail.com>
2025-01-28 09:43:21 +0700

    add code di untuk AktivitasPenyediaViewModel&insert

--- app/src/main/java/com/example/a10_118/ui/viewmodel/Aktivitas/AktivitasPenyediaViewModel.kt
+++ app/src/main/java/com/example/a10_118/ui/viewmodel/Aktivitas/AktivitasPenyediaViewModel.kt
@@ -1,2 +1,29 @@
 package com.example.a10_118.ui.viewmodel.Aktivitas
 
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewmodel.CreationExtras
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+import com.example.a10_118.PertanianApplication
+
+object AktivitasPenyediaViewModel {
+    val Factory = viewModelFactory {
+        initializer {
+            HomeAktivitasViewModel(
+                aplikasiPertanian()
+                    .container.aktivitasRepository
+            )
+        }
+
+        initializer {
+            InsertAktivitasViewModel(
+                aplikasiPertanian()
+                    .container.aktivitasRepository
+            )
+        }
+    }
+
+    fun CreationExtras.aplikasiPertanian():PertanianApplication =
+        (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as PertanianApplication)
+
+}
--- app/src/main/java/com/example/a10_118/ui/viewmodel/Aktivitas/InsertCatatanViewModel.kt
+++ app/src/main/java/com/example/a10_118/ui/viewmodel/Aktivitas/InsertCatatanViewModel.kt
@@ -1,2 +1,66 @@
 package com.example.a10_118.ui.viewmodel.Aktivitas
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.a10_118.Model.AktivitasPertanian
+import com.example.a10_118.Repository.AktivitasRepository
+import kotlinx.coroutines.launch
+
+class InsertAktivitasViewModel(private val aktivitasPertanian: AktivitasRepository) : ViewModel() {
+    var uiState by mutableStateOf(InsertUiState())
+        private set
+
+    fun updateInsertAktivitasState(insertUiEvent: InsertUiEvent) {
+        uiState = InsertUiState(insertUiEvent = insertUiEvent)
+    }
+
+    suspend fun insertAktivitas() {
+        viewModelScope.launch {
+            uiState = uiState.copy(isLoading = true)
+            try {
+                aktivitasPertanian.insertAktivitas(uiState.insertUiEvent.toAktivitas())
+                uiState = uiState.copy(isSuccess = true, isLoading = false) // Update state success
+            } catch (e: Exception) {
+                uiState = uiState.copy(isLoading = false) // Handle error
+                e.printStackTrace()
+            }
+        }
+    }
+}
+
+data class InsertUiState(
+    val insertUiEvent: InsertUiEvent = InsertUiEvent(),
+    val isLoading: Boolean = false,
+    val isSuccess: Boolean = false
+)
+
+data class InsertUiEvent(
+    val id_aktivitas: String ="",
+    val id_tanaman: String = "",
+    val id_pekerja: String="",
+    val tanggal_aktivitas: String="",
+    val deskripsi_aktivitas:String=""
+
+)
+
+fun InsertUiEvent.toAktivitas(): AktivitasPertanian = AktivitasPertanian(
+    id_aktivitas =id_aktivitas,
+    id_tanaman = id_tanaman,
+    id_pekerja = id_pekerja,
+    tanggal_aktivitas = tanggal_aktivitas,
+    deskripsi_aktivitas = deskripsi_aktivitas
+)
+
+fun AktivitasPertanian.toUiStateTanaman(): InsertUiState = InsertUiState(
+    insertUiEvent = toInsertUiEvent()
+)
+
+fun AktivitasPertanian.toInsertUiEvent(): InsertUiEvent = InsertUiEvent(
+    id_aktivitas=id_aktivitas,
+    id_tanaman = id_tanaman,
+    id_pekerja = id_pekerja,
+    tanggal_aktivitas=tanggal_aktivitas,
+)
